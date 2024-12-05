2a0f822defea9673de76be2bde43cdb33d8f2f9a
maksimir91 <maksimirrus@gmail.com>
2024-12-05 15:41:26 +0300

    Создал главный экран с кнопками и добавил navigation controller

--- LanguageLearner/SceneDelegate.swift
+++ LanguageLearner/SceneDelegate.swift
@@ -11,13 +11,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        guard let windowScene = scene as? UIWindowScene else { return false }
+        
+        window = UIWindow(windowScene: windowScene)
+        let mainViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
     }
+
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+//        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+//        guard let _ = (scene as? UIWindowScene) else { return }
+//    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
--- LanguageLearner/ViewController.swift
+++ LanguageLearner/ViewController.swift
@@ -11,9 +11,57 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
-
+    private func setupUI() {
+        // Устанавливаем цвет фона
+        view.backgroundColor = .white
+        // Создаем заголовок
+        let titleLable = UILabel()
+        titleLable.translatesAutoresizingMaskIntoConstraints = false
+        titleLable.text = "Language Learner"
+        titleLable.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        titleLable.textAlignment = .center
+        
+        // Кнопки
+        let flashCardsButton = createButton(withTitle: "Флэш-карточки")
+        let addWordsButton = createButton(withTitle: "Добавить слова")
+        let testButton = createButton(withTitle: "Тесты")
+        
+        // Стек для упрощения расположения
+        let stackView = UIStackView(arrangedSubviews: [titleLable, flashCardsButton, addWordsButton, testButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.alignment = .center
+     
+        // Добавляем стек на экран
+        view.addSubview(stackView)
+        
+        // Устанавливаем констрейнты
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            flashCardsButton.widthAnchor.constraint(equalToConstant: 200),
+            addWordsButton.widthAnchor.constraint(equalToConstant: 200),
+            testButton.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    // Метод для создания кнопок
+    private func createButton(withTitle title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        return button
+    }
+    
 }
 
